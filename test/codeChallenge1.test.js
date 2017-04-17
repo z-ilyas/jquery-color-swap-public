07688c2bcb9534a034b9e4532f5a7ec91728502a
Millie Walsh <millie@primeacademy.io>
2017-04-17 10:23:35 -0500

    fix test for swaping back to red if clicked twice

--- test/codeChallenge1.test.js
+++ test/codeChallenge1.test.js
@@ -69,13 +69,13 @@ describe('basemode 1_selectors assignment', function() {
       onReady();
       //click create and then swap the color twice
       $('#create').click();
-      $('.swap').click();
-      $('.swap').click();
+      $('.swap').click(); // should be yellow
+      $('.swap').click(); // should be red again
 
       //the background-color: red won't be detected if in css stylesheet.
       // check that background-color is not still yellow
-      var redDiv = $('.color-div').css('background-color') != 'rgb(255, 255, 0)';
-      assert.equal(redDiv, false, 'background-color should not be yellow');
+      var divColor = $('.color-div').css('background-color');
+      assert.equal(divColor, 'rgb(255, 0, 0)', 'background-color should be red');
 
     });
 
@@ -131,22 +131,3 @@ describe('basemode 1_selectors assignment', function() {
     });
   })
 });
-
-// Description:
-// Follow the steps below to complete the code challenge:
-//
-// Create a <button> element on the DOM, you may do this in jQuery, or write it directly in the HTML code. Have the button text read Generate.
-//
-// Using jQuery, append a <div> element when you click the button.
-//
-// Inside the <div> element you created in the previous step, create a <p> element that shows how many times you have clicked the button from the first step.
-//
-// Inside the <div> element you created in step two, append two <button> elements. The text of the two buttons should read Swap and Delete.
-//
-// In a CSS file, any <div> created using the Generate button should have a Red background-color.
-//
-// Clicking a Swap <button> should change its parent background-color from Red to Yellow.
-//
-// Clicking a Delete <button> should remove its parent <div> container (and all of its contents).
-//
-// No need to do anything beyond the challenge described above. Once you are complete, check your code into a GitHub repo and submit via this application. Then quietly leave the room until the challenge is over.
