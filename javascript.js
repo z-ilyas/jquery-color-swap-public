c04c030b18b617ad70788fe35c4619a5f62cfb69
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-03 14:59:52 -0500

    ran out of time

--- javascript.js
+++ javascript.js
@@ -2,13 +2,22 @@ $(document).ready(onReady);
 let counter = 1;
 function onReady() {
     $('#generete-btn').on('click', createElement);
+    $('div').children().children().on('click', makeitYellow);
+    $('div').children().children().children().on('click', deleteDiv)
 }
 
 function createElement(){
     $('Main').append(`<div>
-    <p> ${counter++}</p>
-    <button id="yellow-btn">Yellow</button>
+    <p> ${counter++} </p>
+    <button>Yellow</button>
     <button>Delete</button>
     </div>`);
-    $('#yellow-btn').css('div','background-color', 'yellow');
+}
+
+function makeitYellow() {
+    $('div').css('background-color', 'yellow')
+}
+
+function deleteDiv() {
+    $(this).parent().remove()
 }
